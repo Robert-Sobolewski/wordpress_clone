a627da736de559046ad8f05e5b35dbf2d3c8564e
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-03-30 15:17:57 +0200

    fix Home page UI

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import ToggleTab from "../components/toggle-tab/ToggleTab";
+import "./Home.scss";
 
 const Home = () => {
   return (
     <Fragment>
       <div className="home">
         <ToggleTab />
-        <h1 style={{ margin: "0.5em" }}>home page</h1>
+        <h1>home page</h1>
       </div>
     </Fragment>
   );
